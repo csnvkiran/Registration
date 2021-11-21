c5f145d3c23a72397aafb70c1a3d45df6e27cfa3
csnvkiran <csnvkiran@gmail.com>
2021-11-21 22:35:57 +0400

    1 file modified

--- src/app/pages/shared-forms/shared-forms.module.ts
+++ src/app/pages/shared-forms/shared-forms.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { RegistrationsModule } from './features/registrations/registrations.module';
-
 import { NumberInputComponent } from './shared/app-form/number-input/number-input.component';
 import { CounterInputComponent } from './shared/app-form/counter-input/counter-input.component';
 import { CountrySelectorComponent } from './shared/app-form/country-selector/country-selector.component';
