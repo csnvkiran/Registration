2c311d19b0927eb90c0d449275b7c840313a868b
csnvkiran <csnvkiran@gmail.com>
2021-11-21 21:14:06 +0400

    2 files modified, 26 added, 25 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { RegistrationsModule } from './modules/registrations/registrations.module';
-import { CounterInputComponent } from './shared/counter-input/counter-input.component';
-import { NumberInputComponent } from './shared/number-input/number-input.component';
-import { CountrySelectorComponent } from './shared/country-selector/country-selector.component';
-import { InfoInputComponent } from './shared/info-input/info-input.component';
+import { RegistrationsModule } from './features/registrations/registrations.module';
+
+import { NumberInputComponent } from './shared/app-form/number-input/number-input.component';
+import { CounterInputComponent } from './shared/app-form/counter-input/counter-input.component';
+import { CountrySelectorComponent } from './shared/app-form/country-selector/country-selector.component';
+import { InfoInputComponent } from './shared/app-form/info-input/info-input.component';
 
 @NgModule({
   imports: [
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,10 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot([])],
+  exports: [RouterModule],
+  providers: [],
+})
+export class AppRoutingModule {}
