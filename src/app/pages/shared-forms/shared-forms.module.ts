6a08293ec6c441771304803c364933f4ff964fbd
csnvkiran <csnvkiran@gmail.com>
2021-11-21 22:30:03 +0400

    2 files modified

--- src/app/pages/shared-forms/shared-forms.module.ts
+++ src/app/pages/shared-forms/shared-forms.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { SharedFormsComponent } from './pages/shared-forms/shared-forms.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-const routes: Routes = [{ path: '', component: SharedFormsComponent }];
+import { RegistrationsModule } from './features/registrations/registrations.module';
+
+import { NumberInputComponent } from './shared/app-form/number-input/number-input.component';
+import { CounterInputComponent } from './shared/app-form/counter-input/counter-input.component';
+import { CountrySelectorComponent } from './shared/app-form/country-selector/country-selector.component';
+import { InfoInputComponent } from './shared/app-form/info-input/info-input.component';
+import { ShareformsRoutingModule } from './shared-forms-routing.module';
+import { SharedFormsComponent } from './shared-forms.component';
+
 
 @NgModule({
-  declarations: [],
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [],
+  imports: [
+    FormsModule,
+    BrowserAnimationsModule,
+    ShareformsRoutingModule,
+  ],
+  declarations: [
+    CounterInputComponent,
+    NumberInputComponent,
+    CountrySelectorComponent,
+    InfoInputComponent,
+  ],
+  bootstrap: [SharedFormsComponent],
 })
-export class AppRoutingModule {}
